b8de0597434fa0001f3766cfd5894e476057374d
Illia Poliakov <illiapoliakov1@gmail.com>
2026-03-05 00:18:35 +0300

    add yandex metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,23 @@ export default function RootLayout({
   return (
     <html lang={locale}>
       <body className="font-body">
+        <Script id="yandex-metrika-init" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107147477', 'ym');
+
+            ym(107147477, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/107147477" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-PQL2WVYLV2"
           strategy="afterInteractive"
